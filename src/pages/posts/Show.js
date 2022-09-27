706e2b8bab1720eaffaff34acbc0c70984d1ff4b
mostaf jahani <mm.mostafa.mm@gmail.com>
2022-09-27 04:38:41 -0700

    set update post

--- src/pages/posts/Show.js
+++ src/pages/posts/Show.js
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom"
+import {Link, useParams} from "react-router-dom"
 
 const ShowPost = () => {
     const {postId} = useParams();
@@ -35,6 +35,9 @@ const ShowPost = () => {
                     <ul className="card-body">
                         {post.body}
                     </ul>
+                    <div className="card-footer">
+                        <Link className="btn btn-sm btn-dark" to={`/posts/edit/${postId}`}>Edit</Link>
+                    </div>
                 </div>
             </div>}
         </>
